44cd45a516ea31a306b5ca22b614116f454d1bc2
Jim Phelps <jphelps@primera.net>
2023-01-11 08:05:20 -0700

    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,10 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
   name: "primera-38255",
   url: "https://primera-38255.botics.co",
   api: "https://primera-38255.botics.co/api/v1"
-}
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
